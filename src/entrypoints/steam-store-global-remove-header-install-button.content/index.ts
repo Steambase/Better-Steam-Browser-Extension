c6deed6053c3c4c0ea800cfbb43dbf357df64dc4
Lucas Wyland <lucaswyland11@gmail.com>
2024-04-07 12:21:18 -0500

    Added Global Remove Install Button

---
+++ src/entrypoints/steam-store-global-remove-header-install-button.content/index.ts
@@ -0,0 +1,22 @@
+export default defineContentScript({
+  matches: ["*://store.steampowered.com/*"],
+
+  main(ctx) {
+    const ui = createIntegratedUi(ctx, {
+      position: "inline",
+      onMount: (_) => {
+        // Try Get Install Button
+        const installBtn = document.querySelector("#global_action_menu > a.header_installsteam_btn");
+        if (!installBtn) {
+          console.warn(`[steam-store-global-remove-header-install-button] - Unable to find install button`);
+          return;
+        }
+
+        // Remove From DOM
+        installBtn.remove();
+      },
+    });
+
+    ui.mount();
+  },
+});

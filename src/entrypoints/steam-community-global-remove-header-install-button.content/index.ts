9fd421db308eeeb4bb4f49f25959836ddf6ed3c0
Lucas Wyland <lucaswyland11@gmail.com>
2024-04-07 13:14:25 -0500

    Added Remove Install Button on Community Hub

---
+++ src/entrypoints/steam-community-global-remove-header-install-button.content/index.ts
@@ -0,0 +1,21 @@
+export default defineContentScript({
+  matches: ["*://steamcommunity.com/*"],
+  main(ctx) {
+    const ui = createIntegratedUi(ctx, {
+      position: "inline",
+      onMount: (_) => {
+        // Try Get Install Button
+        const installBtn = document.querySelector("#global_action_menu > a.header_installsteam_btn");
+        if (!installBtn) {
+          console.warn(`[steam-community-global-remove-header-install-button] - Unable to find install button`);
+          return;
+        }
+
+        // Remove From DOM
+        installBtn.remove();
+      },
+    });
+
+    ui.mount();
+  },
+});

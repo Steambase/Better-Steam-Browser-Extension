9515fa2902ad3d68a384e77c59f2ba9bee396d93
Lucas Wyland <lucaswyland11@gmail.com>
2024-04-09 13:14:31 -0500

    Installed Button

--- src/entrypoints/popup/App.svelte
+++ src/entrypoints/popup/App.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import svelteLogo from "../../assets/svelte.svg";
+  import Button from "$lib/components/ui/button/button.svelte";
 </script>
 
 <main>
   <h1 class="text-3xl font-bold underline">WXT + Svelte</h1>
   <p class="read-the-docs">Click on the WXT and Svelte logos to learn more</p>
+
+  <Button on:click={() => console.log("clicked")}>Click me</Button>
 </main>

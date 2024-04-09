1d09652443f4dc498428ea93925504c7e5857534
Lucas Wyland <lucaswalter@users.noreply.github.com>
2024-04-09 13:34:58 -0500

    feature: added shadcn-svelte and added icons (#1)
    * Installed Tailwind CSS
    
    * (WIP) - Deps
    
    * Manual Shad CN Svelte Config
    
    * Init Shadcn Svelte
    
    * Installed Button
    
    * Revert "Installed Button"
    
    This reverts commit 9515fa2902ad3d68a384e77c59f2ba9bee396d93.
    
    * Added Icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "wxt";
 import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
@@ -5,7 +6,8 @@ import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
   srcDir: "src",
   manifest: {
-    host_permissions: ["*://*.steambase.io/*", "*://store.steampowered.com/*"],
+    host_permissions: ["*://*.steambase.io/*", "*://store.steampowered.com/*", ""],
+    permissions: ["storage"],
   },
   vite: () => ({
     plugins: [
@@ -15,5 +17,10 @@ export default defineConfig({
         preprocess: [vitePreprocess()],
       }),
     ],
+    resolve: {
+      alias: {
+        $lib: path.resolve("./src/lib"),
+      },
+    },
   }),
 });

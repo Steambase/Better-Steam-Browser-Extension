c51624172faa0bfe53083945e06f7c30d0e18ff9
Lucas Wyland <lucaswyland11@gmail.com>
2024-04-08 22:41:33 -0500

    Added Bypass Link Filter Script

---
+++ src/entrypoints/steam-global-bypass-link-filter.content/index.ts
@@ -0,0 +1,27 @@
+export default defineContentScript({
+  matches: ["*://store.steampowered.com/*", "*://steamcommunity.com/*"],
+  main(ctx) {
+    const ui = createIntegratedUi(ctx, {
+      position: "inline",
+      onMount: (_) => {
+        const linkFilterAnchors = document.querySelectorAll('a[href^="https://steamcommunity.com/linkfilter/"]') as NodeListOf<HTMLAnchorElement>;
+
+        for (const link of linkFilterAnchors) {
+          if (!link.search) {
+            continue;
+          }
+
+          // Replace Each Link Target With Destination
+          const params = new URLSearchParams(link.search);
+          if (params.has("u")) {
+            link.href = params.get("u")!;
+          } else if (params.has("url")) {
+            link.href = params.get("url")!;
+          }
+        }
+      },
+    });
+
+    ui.mount();
+  },
+});

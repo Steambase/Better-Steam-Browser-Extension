8523bbc8be964e3abefe61f70c7820c301f75c59
Lucas Wyland <lucaswyland11@gmail.com>
2024-04-09 13:04:17 -0500

    Installed Tailwind CSS

--- src/entrypoints/popup/App.svelte
+++ src/entrypoints/popup/App.svelte
@@ -3,33 +3,6 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://wxt.dev" target="_blank" rel="noreferrer">
-      <img src="/wxt.svg" class="logo" alt="WXT Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>WXT + Svelte</h1>
-
+  <h1 class="text-3xl font-bold underline">WXT + Svelte</h1>
   <p class="read-the-docs">Click on the WXT and Svelte logos to learn more</p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #54bc4ae0);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>

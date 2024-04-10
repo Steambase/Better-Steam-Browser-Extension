0e268f4b1838bb4b1cb0d24d760f82a9defc9ab5
Lucas Wyland <lucaswyland11@gmail.com>
2024-04-09 21:40:59 -0500

    Lowered Description

--- wxt.config.ts
+++ wxt.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   manifest: {
     name: "Better Steam",
     description:
-      "Improve your experience using Steam — Better Steam removes the bloat, adds game insights, and helps you hunt down the best deal for your next game purchase.",
+      "Improve your experience on Steam — Better Steam removes the bloat, adds game insights, and helps you hunt down the best deal for your next purchase.",
     host_permissions: ["*://*.steambase.io/*", "*://*.steampowered.com/*", "*://steamcommunity.com/*"],
     permissions: ["storage"],
   },

85f415449d7b455c67f01fdb511d67023bed83c8
Lucas Wyland <lucaswyland11@gmail.com>
2024-04-09 21:05:20 -0500

    Wired Up Install Steam Options

--- src/entrypoints/steam-global-remove-header-install-button.content/index.ts
+++ src/entrypoints/steam-global-remove-header-install-button.content/index.ts
@@ -1,9 +1,18 @@
+import { shouldHideInstallSteamButton } from "@/lib/common/storage/options";
+
 export default defineContentScript({
   matches: ["*://store.steampowered.com/*", "*://steamcommunity.com/*"],
   main(ctx) {
     const ui = createIntegratedUi(ctx, {
       position: "inline",
-      onMount: (_) => {
+      onMount: async (_) => {
+        // Ensure Enabled
+        const enabled = await shouldHideInstallSteamButton.getValue();
+        if (!enabled) {
+          console.log(`[steam-global-remove-header-install-button] - Disabled via user options`);
+          return;
+        }
+
         // Try Get Install Button
         const installBtn = document.querySelector("#global_action_menu > a.header_installsteam_btn") as HTMLAnchorElement;
         if (!installBtn) {
